f12e24dc5773a2480fd263e4b09fee58f4bcc836
clownmonster <mk6386223@gmail.com>
2020-12-27 16:17:28 +0530

    api changes

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -78,7 +78,7 @@ const RegisterRetailer = () => {
           headers: {
           'content-type': 'application/json'
         }}
-        const res  = await axios.post("https://b4fbc68d6b8d.ngrok.io/signup",registerData,config)
+        const res  = await axios.post("https://aac930744096.ngrok.io/signup",registerData,config)
         if(res.data.success === true){
           localStorage.setItem('log', true)
           localStorage.setItem('name', registerData.username)
@@ -99,7 +99,7 @@ const RegisterRetailer = () => {
           headers: {
           'content-type': 'application/json'
         }}
-        const res  = await axios.post("https://b4fbc68d6b8d.ngrok.io/login",signInData,config)
+        const res  = await axios.post("https://aac930744096.ngrok.io/login",signInData,config)
         console.log(res)
         if(res.data.success === true){
           localStorage.setItem('log', true)
